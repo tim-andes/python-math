fdcda3a18fd39da0dd519bb4adabd476143f4c32
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-11-04 14:58:32 -0700

    first commit

---
+++ test-math.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,3), 5)
+        self.assertEqual(add(1,-1) 0)
+        self.assertEqual(add(-1000, -2000) -3000)
