2b3186736385253cb84aaee82bd5c9287cf5a994
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-11-04 16:12:00 -0700

    add docstring to unit testing

--- test_math_class.py
+++ test_math_class.py
@@ -2,7 +2,21 @@ import unittest
 from math_class import add
 
 class TestCalculator(unittest.TestCase):
+    """
+    A test class for the `add` function in the `math_class` module.
+
+    This class contains unit tests to verify the correct behavior of the `add` function for various input values.
+    """
     def test_add(self):
+        """
+        Tests the `add` function with different input values.
+
+        This test method covers the following scenarios:
+        - Adding two positive integers.
+        - Adding a positive and a negative integer.
+        - Adding two negative integers.
+        - Adding two non-numeric strings (expected to raise a TypeError).
+        """
         self.assertEqual(add(2,3), 5)
         self.assertEqual(add(1,-1), 0)
         self.assertEqual(add(-1000, -2000), -3000)
