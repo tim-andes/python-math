c68e23f54625275538d5efa3cc05af65e758b46a
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-11-05 06:42:18 -0700

    create unittest subtraction

--- test_math_class.py
+++ test_math_class.py
@@ -1,5 +1,6 @@
 import unittest
 from math_class import add
+from math_class import subtract
 
 class TestCalculator(unittest.TestCase):
     """
@@ -19,8 +20,27 @@ class TestCalculator(unittest.TestCase):
         """
         self.assertEqual(add(2,3), 5)
         self.assertEqual(add(1,-1), 0)
+        self.assertEqual(add(.15,.17), .32)
         self.assertEqual(add(-1000, -2000), -3000)
         self.assertEqual(add("one", "two"), TypeError)
+
+    def test_subtract(self):
+        """
+        Tests the `subtract` function with different input values.
+
+        This test method covers the following scenarios:
+        - Subracting two positive integers.
+        - Subracting two positive integers to equal 0.
+        - Subtracting two float decimals.
+        - Subtracting two negative integers.
+        - Subtracting two non-numeric strings (expected to raise a TypeError).
+        """
+        self.assertEqual(subtract(2,3), -1)
+        self.assertEqual(subtract(1,1), 0)
+        self.assertEqual(subtract(.50,.25), .25)
+        self.assertEqual(subtract(-1000, -2000), 3000)
+        self.assertEqual(subtract("one", "two"), TypeError)
+
 
 if __name__ == '__main__':
     unittest.main() 
